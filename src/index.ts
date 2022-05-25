b1fefc5c739e0ad35075b4681e487a6fb5c0e474
Steven Bennett <steven@budgetbloom.com>
2022-05-25 02:27:43 -0700

    chore: Quick release

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from './actions';
 export * from './analyzers';
 export * from './conditions';
 export { createESModule } from './codegen/code/js/es-module';
+export { generateGitIgnoreConfig } from './codegen/config/git/gitignore';
+export { generateYarnConfig } from './codegen/config/package-managers/yarn';
 export { readFile } from './readers/file/index';
 export { readJSONFile } from './readers/file/json';
 export { readYAMLFile } from './readers/file/yaml';
